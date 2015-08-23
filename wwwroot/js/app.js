697c45a39177c21b5bf228a6064668493c020b7d
Jesse Seymour <Jesse@Jesse-Seymours-MacBook-Pro.local>
2015-08-23 11:08:58 -0400

    pins now being located correctly on map

--- wwwroot/js/app.js
+++ wwwroot/js/app.js
@@ -1,6 +1,7 @@
 (function ($, w, d){
 	var stateArray = [],
 	stateData,
+	currentState = {},
 	selected = false;
 	var tl = new TimelineLite();
 	var t = new TimelineLite();
@@ -8,10 +9,8 @@
 	var init = function(){
 		$('svg path').on('click',function(){
 			var stateName = $(this).attr('id');
-			//selectState($("#" + stateName));
-			alert(document.getElementById(stateName).getBoundingClientRect().width);
+			selectState($("#" + stateName));
 		})
-		console.log(document.getElementById("IA"));
 		//set states to scale 0 and transformOrigin to center
 		TweenMax.set($('.state'),{transformOrigin: "50% 50% 0"});
 		//TweenMax.to($('.state'),0,{scale:0});
@@ -37,48 +36,30 @@
 		})
 	}
 
-	function setPins(state,element){
-		var pins = state.pins;
-		var counter = 0;
-		$.each(pins, function(){
-			//console.log($(this)[0]);
-			var pin = $('<div>').addClass('pin');
-			pin.attr({'data-id':counter});
-			pin.css({'left':$(this)[0].x + '%','top':$(this)[0].y + '%'});
-			//in.css('left',$(this));
-			element.append(pin);
-			counter++;
-		});
-	}
-
 	function startAnimation() {
 		//animate in states
 		//tl.staggerTo(stateArray, .8, {scale:1, ease: Back.easeOut}, 0.2);
 
 		//river
-		tl.to($('.river'), 4.0, {height:'108%'})
+		//tl.to($('.river'), 4.0, {height:'108%'})
 	}
 
 	function selectState(state){
+
 		if (!selected){
+			selected = true;
 			state.attr('data-active','1');
+			currentState.state = state.attr('id');
 			t = new TimelineLite({
 				onComplete:function(){
 					var offset = state.offset();
-					var pins;
-					console.log(state.getBoundingClientRect());
+					//var pins;
+					//console.log(state.getBoundingClientRect());
 					//set pins
 					for (i = 0; i < stateData.states.length; i++){
 						if (state.attr('id') == stateData.states[i].abbrev){
-							pins = stateData.states[i].pins;
-							$.each(pins,function(){
-								console.log($(this)[0].x);
-								var pin = $('.pin').clone().appendTo('.svgContainer').css({
-									'left':$(this)[0].x + "%",
-									'top':$(this)[0].y + "%"
-								});
-								
-							})
+							currentState.pins = stateData.states[i].pins;
+							createPins(); //place pins on selected state
 							return;
 						}
 					}
@@ -103,7 +84,7 @@
 			var y = state.attr('data-y');
 			var scale = state.attr('data-scale');
 			t.to(state,.5,{scale:scale,xPercent:x,yPercent:y, svgOrigin: "0 0"});
-			selected = true;
+			
 		} else {
 			t.reverse();
 			state.attr('data-active','0');
@@ -113,6 +94,45 @@
 		//console.log("click");
 	}
 
+	function createPins(){
+		var pins = currentState.pins;
+		var counter = 0;
+		$.each(pins, function(){
+			var pin = $("<div>")
+				.addClass('pin pin_' + currentState.state)
+				.attr({
+					'data-pinid':counter,
+					'data-x':$(this)[0].x,
+					'data-y':$(this)[0].y
+					})
+				.css({
+					'left':0,
+					'top':0
+				});
+			$('.container').append(pin);
+			counter++;
+		})
+		setPins();
+	}
+
+	function setPins(){
+		var path = document.getElementById(currentState.state);
+		var width = path.getBoundingClientRect().width;
+		var height = path.getBoundingClientRect().height;
+		var offset = $("#" + currentState.state).offset();
+		$.each($('.pin_' + currentState.state), function(){
+			var percX = width * $(this).attr('data-x');
+			var percY = height * $(this).attr('data-y');
+			var newLeft = offset.left + percX - ($(this).width() / 2);
+			var newTop = offset.top + percY - $(this).height();
+
+			$(this).css({
+				'left':newLeft,
+				'top':newTop
+			})
+		})
+	}
+
 	function resize() {
 		var s = $('.states');
 		var maxHeight = 755;
@@ -120,24 +140,11 @@
 		var currW = s.width();
 		var currH = s.height();
 		var ratio = 0;
-		// 
-		// var maxHeight = 755;
-		// var maxWidth = 709;
-		// var ratio = maxHeight / maxWidth;
-		// s.height(currW * ratio);
-
-		// if(currH > $('.container').height() * 0.8){
-		// 	s.addClass('alt');
-		// 	ratio = maxHeight / maxWidth;
-		// 	s.height(currW * ratio);
-		// 	return;
-		// }
-
-		//s.removeClass('alt');
 		var ratio = maxWidth / maxHeight;
 		s.width(currH * ratio);
-
-
+		if(selected){
+			setPins();
+		}
 	}
 
 	
